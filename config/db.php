0df415031134e114dc50c2da6ea02096abdbc07e
tbq <tbq2616@sina.com>
2020-04-29 13:43:39 +0800

    ii

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=192.168.99.1;dbname=yii-basic-blog',
-    'username' => 'root',
-    'password' => 'root',
+    'dsn' => 'mysql:host=192.168.56.1;dbname=yii-basic-blog',
+    'username' => 'work',
+    'password' => '111111',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
